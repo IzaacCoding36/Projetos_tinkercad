1ca31718d19fcc7e14608fdd7a3c69d565615d47
Izaac (Zack) <111022921+Zackcoding36@users.noreply.github.com>
2023-09-06 01:42:48 -0300

    Display de 7 elementos
    O código para o display de 7 elementos/segmentos foi adicionado

---
+++ Display7elementos.cpp
@@ -0,0 +1,60 @@
+// Código C++ //
+
+//Definições//
+void setup()
+  
+{ pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);}
+
+void loop()
+{ digitalWrite(13, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(8, HIGH);
+  delay(1500);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(13, LOW);
+  delay(1500);
+  digitalWrite(13, HIGH);
+  digitalWrite(11, LOW);
+  digitalWrite(7, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  delay(1500);
+  digitalWrite(9, LOW);
+  digitalWrite(11, HIGH);
+  delay(1500);
+  digitalWrite(13, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(8, HIGH);
+  delay(1500);
+  digitalWrite(12, LOW);
+  digitalWrite(10, HIGH);
+  digitalWrite(13, HIGH);
+  delay(1500);
+  digitalWrite(12, LOW);
+  digitalWrite(9, HIGH);
+  delay(1500);
+  digitalWrite(12, HIGH);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(10, LOW);
+  delay(1500);
+  digitalWrite(10, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(7, HIGH);
+  delay(1500);
+  digitalWrite(9, LOW);
+  delay(1500);
+  digitalWrite(7, LOW);}
