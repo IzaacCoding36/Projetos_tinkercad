8873527a956da833ad60e2450544cd1d9799149d
Izaac (Zack) <111022921+Zackcoding36@users.noreply.github.com>
2023-10-03 08:42:21 -0300

    Projeto Adicionado

---
+++ Projetos/Sensor-Movimento+Som.cpp
@@ -0,0 +1,32 @@
+//Código C++//
+
+int distancia = 0;
+
+long readUltrasonicDistance(int triggerPin, int echoPin)
+{ pinMode(triggerPin, OUTPUT);
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  return pulseIn(echoPin, HIGH);}
+
+void setup()
+{ pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(10, OUTPUT);}
+
+void loop()
+{ distancia = 0.01723 * readUltrasonicDistance(7, 6);
+  if (distancia < 50) {
+    digitalWrite(13, LOW);
+    digitalWrite(12, HIGH);
+    tone(10, 523, 10000);
+  } else {
+    digitalWrite(13, HIGH);
+    digitalWrite(12, LOW);
+    noTone(10);
+  }
+  delay(10);
+}
