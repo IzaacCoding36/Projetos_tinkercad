ce1045c53a47ba3335c5d445d0a5fb92e17dee3a
Izaac (Zack) <111022921+Zackcoding36@users.noreply.github.com>
2023-10-23 09:12:30 -0300

    Projeto adicionado

---
+++ Projetos/Motor-CC.cpp
@@ -0,0 +1,24 @@
+//Código C++//
+
+void setup()
+{ pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);}
+
+void loop()
+{ analogWrite(11, 200);
+  digitalWrite(10, HIGH);
+  digitalWrite(9, LOW);
+  delay(5000);
+  analogWrite(11, 0);
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);
+  delay(5000);
+  analogWrite(11, 200);
+  digitalWrite(10, LOW);
+  digitalWrite(9, HIGH);
+  delay(5000);
+  analogWrite(11, 0);
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);
+  delay(5000);}
